434a19721a13a932e8d92d9041b34ab643a9de4e
azhar-danish <azhar.danish9@gmail.com>
2026-09-22 13:33:53 +0530

    update the code

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+
 require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/health',(req,res)=>{
-    res.send("Server is healthy and running fine!")
+app.get('/health', (req, res) => {
+    res.send("Server is healthy and running fine!");
 });
 
-app.get('/',(req,res)=>{
-    console.log("Github Action CI/CD for Node.js Application is working fine!")
-    res.send("Github Action CI/CD for Node.js Application is working fine!")
+app.get('/', (req, res) => {
+    console.log("Github Action CI/CD for Node.js Application is working fine!");
+    res.send("Github Action CI/CD for Node.js Application is working fine!");
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on PORT:${PORT}`)
-})
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
+
