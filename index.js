4f9a12841e1684e04bac1804b658f2cc848deea8
azhar-danish <azhar.danish9@gmail.com>
2026-09-24 00:38:16 +0530

    update the routes

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.get('/', (req, res) => {
     res.send("Github Action CI/CD for Node.js Application is working fine!");
 });
 
+app.get('/about', (req, res) => {
+    console.log("About page is working fine!");
+    res.send("About page is working fine!");
+});
+
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on PORT: ${PORT}`);
 });
